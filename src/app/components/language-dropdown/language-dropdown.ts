00f53626bde259ecc55bf42da8c1b7606b47b3d1
iamkorniichuk <iamkorniichuk@gmail.com>
2025-08-12 14:28:38 +0200

    feat: add language routes

--- src/app/components/language-dropdown/language-dropdown.ts
+++ src/app/components/language-dropdown/language-dropdown.ts
@@ -1,7 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { ClickOutside } from '../../directives/click-outside';
-import { TranslateService } from '@ngx-translate/core';
-import { AVAILABLE_LANGUAGES, FALLBACK_LANGUAGE, Language } from '../../language.config';
+import { LanguageService, Language } from '../../services/language';
 
 @Component({
   selector: 'app-language-dropdown',
@@ -10,50 +9,22 @@ import { AVAILABLE_LANGUAGES, FALLBACK_LANGUAGE, Language } from '../../language
   styleUrl: './language-dropdown.css'
 })
 export class LanguageDropdown {
-  private translate = inject(TranslateService);
+  private languageService = inject(LanguageService);
+  readonly availableLanguages = Object.values(this.languageService.availableLanguages);
+  selectedLanguage = this.languageService.selectedLanguage;
+
   isDropdownOpen = signal(false);
-
-  readonly availableLanguages = AVAILABLE_LANGUAGES;
-  selectedLanguage = FALLBACK_LANGUAGE
-  private STORAGE_KEY = 'language';
-
-  constructor() {
-    const languageCodes = AVAILABLE_LANGUAGES.map(l => l.code);
-    this.translate.addLangs(languageCodes);
-    this.translate.setFallbackLang(FALLBACK_LANGUAGE.code);
-
-    const storedLanguageCode = localStorage.getItem(this.STORAGE_KEY);
-    const browserLanguageCode = navigator.language.split('-')[0].toLocaleLowerCase();
-
-    const translateLanguages = this.translate.getLangs(); 
-    let currentLanguageCode = "";
-    if (storedLanguageCode && translateLanguages.find(code => code === storedLanguageCode)) {
-      currentLanguageCode = storedLanguageCode;
-    } else if (browserLanguageCode  && translateLanguages.find(code => code === browserLanguageCode)) {
-      currentLanguageCode = browserLanguageCode;
-    }
-
-    const currentLanguage = AVAILABLE_LANGUAGES.find(l => l.code === currentLanguageCode) || FALLBACK_LANGUAGE;
-    this.applyLanguage(currentLanguage);
-  }
   
   toggleDropdown() {
-    this.isDropdownOpen.update((value) => !value);
+    this.isDropdownOpen.update((isOpen) => !isOpen);
   }
   
-  private applyLanguage(language: Language) {
-    this.selectedLanguage = language;
-    this.translate.use(language.code);
-
-    localStorage.setItem(this.STORAGE_KEY, language.code);
+  close() {
+    this.isDropdownOpen.set(false);
   }
 
   selectLanguage(language: Language) {
-    this.applyLanguage(language);
-    this.isDropdownOpen.set(false);
-  }
-
-  close() {
-    this.isDropdownOpen.set(false);
+    this.languageService.setLanguage(language.code);
+    this.close();
   }
 }
---
+++ src/app/services/language.ts
@@ -0,0 +1,90 @@
+import { Injectable, signal } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+export type Language = {
+  code: string;
+  src: string;
+  label: string;
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService {
+  private LANGUAGE_KEY = 'language';
+  readonly availableLanguages: { [code: string]: Language } = {
+    'en': { code: 'en', src: '/images/flags/united-kingdom.png', label: 'English' },
+    'pl': { code: 'pl', src: '/images/flags/poland.png', label: 'Polski' },
+    'de': { code: 'de', src: '/images/flags/germany.png', label: 'Deutsch' },
+    'cz': { code: 'cz', src: '/images/flags/czech-republic.png', label: 'Čeština' },
+    'ua': { code: 'ua', src: '/images/flags/ukraine.png', label: 'Українська' },
+  };
+  readonly fallbackLanguageCode = 'en';
+  readonly selectedLanguage = signal(this.availableLanguages[this.fallbackLanguageCode]);
+
+  constructor(
+    private translate: TranslateService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.translate.addLangs(Object.keys(this.availableLanguages));
+    this.translate.setFallbackLang(this.fallbackLanguageCode);
+
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => {
+        const routeLanguageCode = this.route.root.firstChild?.snapshot.paramMap.get(this.LANGUAGE_KEY);
+        if (this.isSuitableLanguageCode(routeLanguageCode) && routeLanguageCode !== this.translate.getCurrentLang()) {
+          this.setLanguage(routeLanguageCode);
+        }
+      })
+
+    const routeLanguage = this.route.snapshot.paramMap.get(this.LANGUAGE_KEY);
+    const storedLanguage = localStorage.getItem(this.LANGUAGE_KEY);
+    const browserLanguage = navigator.language.split('-')[0].toLowerCase();
+  
+    let languageCode = this.fallbackLanguageCode;
+    if (this.isSuitableLanguageCode(routeLanguage)) {
+      languageCode = routeLanguage;
+    } else if (this.isSuitableLanguageCode(storedLanguage)) {
+      languageCode = storedLanguage;
+    } else if (this.isSuitableLanguageCode(browserLanguage)) {
+      languageCode = browserLanguage;
+    }
+
+    this.setLanguage(languageCode);
+    this.navigateToLanguage();
+  }
+
+  setLanguage(languageCode: string) {
+    const language = this.availableLanguages[languageCode];
+    if (!language) return;
+
+    this.selectedLanguage.set(language);
+    this.translate.use(languageCode);
+    localStorage.setItem(this.LANGUAGE_KEY, languageCode);
+  }
+
+  navigateToLanguage() {
+    const languageCode = this.selectedLanguage().code;
+    const url = this.router.url;
+
+    const segments = url.split('/').filter(Boolean);
+    const routeLanguageCode = this.route.snapshot.paramMap.get(this.LANGUAGE_KEY);
+
+    if (routeLanguageCode === languageCode) return;
+
+    if (this.isSuitableLanguageCode(routeLanguageCode)) {
+      segments.shift();
+    }
+
+    const newSegments = [languageCode, ...segments];
+    this.router.navigate(['/', ...newSegments], {replaceUrl: true})
+  }
+
+  private isSuitableLanguageCode(languageCode: string | null | undefined): languageCode is string {
+    return typeof languageCode === 'string' && languageCode in this.availableLanguages;
+  }
+}
