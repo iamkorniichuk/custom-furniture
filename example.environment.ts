a7f32fcc18fd3874ce9d006eb13b7ae5f33f1e86
iamkorniichuk <iamkorniichuk@gmail.com>
2025-08-26 17:53:40 +0200

    docs: add help on firebase integration

---
+++ example.environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase:
+    {
+        projectId: 'custom-furniture',
+        appId: '1:319379837888:web:f35f4e9ccb15e',
+        storageBucket: 'custom-furniture.firebasestorage.app',
+        apiKey: 'tKCoT7pMsLh8BJxuirknPJEuvS0GHCQlM1tnyYPs',
+        authDomain: 'custom-furniture.firebaseapp.com',
+        messagingSenderId: '567272677385',
+    }
+};
