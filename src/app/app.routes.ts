90517c864e32c2f01dc8e99dad684534de32c8fe
iamkorniichuk <iamkorniichuk@gmail.com>
2025-08-28 10:47:14 +0200

    feat(WIP): add not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
     {
         path: ':language',
         children: [
             {path: '', component: Home},
+            {path: '**', component: NotFound},
         ]
     },
 ];
