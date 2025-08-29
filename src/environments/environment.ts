edf1e583fb3a34a9430238527f800f027673bba4
iamkorniichuk <iamkorniichuk@gmail.com>
2025-08-29 20:43:30 +0200

    feat: connect to firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'iak-custom-furniture',
+    appId: '1:243629319240:web:d12b902e00020955c8e47d',
+    storageBucket: 'iak-custom-furniture.firebasestorage.app',
+    apiKey: 'AIzaSyAee7JUvQ57exX_KmkM8y8SzZxlqxu9dao',
+    authDomain: 'iak-custom-furniture.firebaseapp.com',
+    messagingSenderId: '243629319240',
+  },
+};
