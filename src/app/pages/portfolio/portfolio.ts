882545ae70d5514789beb87efd015f69e8eeb3b2
iamkorniichuk <iamkorniichuk@gmail.com>
2025-09-01 12:56:26 +0200

    feat: add room filtering

--- src/app/pages/portfolio/portfolio.ts
+++ src/app/pages/portfolio/portfolio.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 import portfolioData from '../../../assets/furnitures.json';
 
@@ -9,9 +10,18 @@ interface ImagePaths {
 }
 
 interface Image {
-  orientation: string;
+  orientation: 'portrait' | 'landscape';
   paths: ImagePaths;
 }
+
+type Room =
+  | 'kitchen'
+  | 'kidroom'
+  | 'guestroom'
+  | 'bathroom'
+  | 'bedroom'
+  | 'business'
+  | 'corridor';
 
 interface Project {
   title_cz: string;
@@ -19,15 +29,31 @@ interface Project {
   title_en: string;
   title_pl: string;
   title_ua: string;
+  room: Room;
   images: Image[];
 }
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.css',
 })
 export class PortfolioComponent {
-  portfolio = signal<Project[]>(portfolioData);
+  private portfolio = portfolioData as Project[];
+  selectedRoom = signal<Room | null>(null);
+  roomOptions: Room[] = [
+    'kitchen',
+    'kidroom',
+    'guestroom',
+    'bathroom',
+    'bedroom',
+    'business',
+    'corridor',
+  ];
+
+  projects = computed(() => {
+    if (this.selectedRoom() === null) return this.portfolio;
+    return this.portfolio.filter((row) => row.room === this.selectedRoom());
+  });
 }
